011ff53e60c01a187e138d8427729bb65715d860
Adarsh2003Thapa <100429154+adarshthapa111@users.noreply.github.com>
2024-08-26 09:39:05 +0545

    bug fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
                 )}
               </nav>
               <button className="hidden md:block bg-black text-white px-6 py-2 rounded-full hover:bg-gray-800 transition duration-300">
-                Let's chat
+                Let&apos;s chat
               </button>
               <button
                 className="md:hidden text-black"
@@ -106,10 +106,10 @@ export default function Home() {
                   </button>
                 </div>
                 <div className="flex flex-wrap gap-8 pt-8">
-                  <div className="bg-gray-100 p-4 rounded-lg shadow-md border">
-                    <h2 className="text-4xl font-bold text-black">+84</h2>
+                  <div className="bg-gray-100 p-4 rounded-lg shadow-md border text-center">
+                    <h2 className="text-4xl font-bold text-black">6+</h2>
                     <p className="text-gray-600 text-sm mt-2">
-                      Clients worldwide
+                      Experience
                     </p>
                   </div>
                   <div className="bg-gray-100 p-4 rounded-lg shadow-md border">
